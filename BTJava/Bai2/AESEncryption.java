8292c1dae26bc224f59061d7b426b34d7c7f1fa3
truongchinh2006 <chinhhlt.24ite@vku.udn.vn>
2025-04-14 16:03:43 +0700

    Add files via upload

---
+++ BTJava/Bai2/AESEncryption.java
@@ -0,0 +1,33 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESEncryption implements Encryptable {
+    private SecretKey secretKey;
+
+    public AESEncryption() throws Exception {
+        // Tạo secret key AES
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128); // AES-128
+        this.secretKey = keyGen.generateKey();
+    }
+
+    @Override
+    public String encrypt(String data) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    @Override
+    public String decrypt(String encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decoded = Base64.getDecoder().decode(encryptedData);
+        byte[] decrypted = cipher.doFinal(decoded);
+        return new String(decrypted);
+    }
+}
